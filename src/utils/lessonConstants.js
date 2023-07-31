0fd5240ca97dd3da1430cee6a08ddac722cf2c5e
Joseph Venuto <jbvenuto@gmail.com>
2023-07-31 10:59:02 -0400

    added german lessons

--- src/utils/lessonConstants.js
+++ src/utils/lessonConstants.js
@@ -104,104 +104,104 @@ const lessonConstants = {
                 },
             },
         },
-        // german: {
-        //     'section-1': {
-        //         'German1-1': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German1-2': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German1-3': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German1-4': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German1-5': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German1-6': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German1-7': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German1-8': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German1-9': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //     },
-        //     'section-2': {
-        //         'German2-1': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German2-2': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German2-3': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German2-4': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German2-5': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German2-6': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German2-7': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //     },
-        //     'section-3': {
-        //         'German3-1': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //         'German3-2': {
-        //             locationSrc: '',
-        //             played: false,
-        //             time: 0
-        //         },
-        //     },
-        // }
+        german: {
+            'section-1': {
+                'German1-1': {
+                    locationSrc: 'https://ia601806.us.archive.org/10/items/russian-3-1/German%201-1.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German1-2': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%201-2.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German1-3': {
+                    locationSrc: 'https://ia601806.us.archive.org/10/items/russian-3-1/German%201-3.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German1-4': {
+                    locationSrc: 'https://ia601806.us.archive.org/10/items/russian-3-1/German%201-4.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German1-5': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%201-5.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German1-6': {
+                    locationSrc: 'https://ia601806.us.archive.org/10/items/russian-3-1/German%201-6.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German1-7': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%201-7.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German1-8': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%201-8.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German1-9': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%201-9.mp3',
+                    played: false,
+                    time: 0
+                },
+            },
+            'section-2': {
+                'German2-1': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%202-1.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German2-2': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%202-2.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German2-3': {
+                    locationSrc: 'https://ia601806.us.archive.org/10/items/russian-3-1/German%202-3.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German2-4': {
+                    locationSrc: 'https://ia601806.us.archive.org/10/items/russian-3-1/German%202-4.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German2-5': {
+                    locationSrc: 'https://ia601806.us.archive.org/10/items/russian-3-1/German%202-5.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German2-6': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%202-6.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German2-7': {
+                    locationSrc: 'https://ia601806.us.archive.org/10/items/russian-3-1/German%202-7.mp3',
+                    played: false,
+                    time: 0
+                },
+            },
+            'section-3': {
+                'German3-1': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%203-1.mp3',
+                    played: false,
+                    time: 0
+                },
+                'German3-2': {
+                    locationSrc: 'https://ia801806.us.archive.org/10/items/russian-3-1/German%203-2.mp3',
+                    played: false,
+                    time: 0
+                },
+            },
+        }
     }
 }
 
